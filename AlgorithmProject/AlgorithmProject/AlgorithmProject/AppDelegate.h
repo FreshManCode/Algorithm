85d4c364fa55f5bd7f69b6a61af771a109ee80d8
张君君 <649283213@qq.com>
2020-03-26 15:05:41 +0800

    release twocd

---
+++ AlgorithmProject/AlgorithmProject/AlgorithmProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlgorithmProject
+//
+//  Created by 张君君 on 2020/3/26.
+//  Copyright © 2020年 com.zhangjunjun.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
